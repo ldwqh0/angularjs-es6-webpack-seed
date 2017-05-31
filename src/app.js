edd68d455087e72f4d1da875e8166d97a2e30d0c
xz <ldwqh0@outlook.com>
2017-05-31 11:36:01 +0800

    调整入口文件

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 // 导入全局样式
 import './css/style.css'
+// 导入项目全局依赖
 import 'es6-promise/auto'
 import angular from 'angular'
 import uiRouter from '@uirouter/angularjs'
 import oclazyload from 'oclazyload'
-
+// 导入路由配置
 import route from './router'
 const app = angular.module('app', [uiRouter, oclazyload])
 app.config(route)
