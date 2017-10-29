a8277c139aa6d222b9a4f80df6033b4bfaf22bd3
xz <ldwqh0@outlook.com>
2017-10-29 22:26:00 +0800

    修正eslint错误

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
   env: {
     browser: true,
   },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
   plugins: [
     'html'
   ],
