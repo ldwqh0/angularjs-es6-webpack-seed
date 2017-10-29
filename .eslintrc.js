4ed83575fc6ab3a7e8576720118efb1508cb4f9f
xz <ldwqh0@outlook.com>
2017-10-29 12:07:44 +0800

    替换polyfill为babel-polyfill 升级webpack 为3.8.x

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,6 @@ module.exports = {
   env: {
     browser: true,
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.vue files
   plugins: [
     'html'
   ],
